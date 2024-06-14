b8b9b211702da46ec19910c6f6404fc0899f92b8
Vedant Banaitkar <vedantbanaitkar@gmail.com>
2024-06-14 23:16:13 +0530

    Till video 75
    14-06-2024

--- Exercise 13/script.js
+++ Exercise 13/script.js
@@ -17,9 +17,43 @@ function setViews(views){
 }
 
 function setMonthsOld(monthsOld){
-    document.querySelector(".info span:nth-child(6)").textContent = monthsOld;
+    document.querySelector(".info span:nth-child(6)").textContent = `${monthsOld} months ago`;
 }
 
 function setThumbnail(url){
     document.querySelector(".image img").setAttribute("src", url);
 }
+
+function setDuration(duration){
+    document.querySelector(".duration").textContent=duration;
+}
+
+function calcViews(views){
+    if (views < 1000) {
+      return views;
+    } else if (views < 1000000) {
+      views = views / 1000;
+      return `${views}K`;
+    } else if (views > 1000000) {
+      views = views / 1000000;
+      return `${views}M`;
+    }
+    else{
+        return views;
+    }
+}
+function setter(){
+    let title = prompt("Enter title of a video: ");
+    let cName = prompt("Enter Channel Name: ");
+    let views = calcViews(Number(prompt("Enter no. of Views: ")));
+    let monthsOld = Number(prompt("Enter how many months old: "));
+    let duration = prompt("Enter video duration: ");
+    setTitle(title);
+    setCName(cName);
+    setViews(views);
+    setMonthsOld(monthsOld);
+    setDuration(duration);
+
+}
+
+setter();
