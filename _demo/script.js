44ce6be0265bc2de53b28a1d46af0807ff6a562f
Vedant Banaitkar <vedantbanaitkar@gmail.com>
2024-06-15 23:11:56 +0530

    till video 75
    15-06-2024

--- _demo/script.js
+++ _demo/script.js
@@ -1,2 +1,10 @@
-let box = (document.body.querySelector(".box").hidden = true);
-console.log(box.hasAttribute("hidden"));
+// let box = (document.body.querySelector(".box").hidden = true);
+// console.log(box.hasAttribute("hidden"));
+
+console.log("start");
+
+setInterval(() => {
+    console.log("timeout")
+}, 100);
+
+console.log("End");
