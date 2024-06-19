c7166d91fffd3c69e17800946bbde14449dc7bc4
Vedant Banaitkar <vedantbanaitkar@gmail.com>
2024-06-19 23:28:46 +0530

    Bs update
    19-06-2024

--- JS MiniProjects/Project 4 - Guess the Number/chaiaurcode.js
+++ JS MiniProjects/Project 4 - Guess the Number/chaiaurcode.js
@@ -1,3 +1,14 @@
-function genRandom(){
-  return (Math.random()*100)+1;
-}
+const randomNumber = parseInt(Math.random()*100 +1);
+
+const submit = document.querySelector("#subt");
+const userInput = document.querySelector("#guessField");
+const guessSlot = document.querySelector(".guesses");
+const remaining = document.querySelector(".lastResult");
+const lowOrHi = document.querySelector(".lowOrHi");
+
+const p = document.createElement("p");
+
+let prevGuess = [];
+let numGuess = 1;
+
+let playGame = true;
