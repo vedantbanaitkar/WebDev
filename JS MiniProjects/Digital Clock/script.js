a9e5ccdf73feeb1868065df0a2738597b82e3389
Vedant Banaitkar <vedantbanaitkar@gmail.com>
2024-06-22 23:19:20 +0530

    Building some fun projects
    22-06-2024
    going through Namaste Javascript (the best js playlist acc to me)

---
+++ JS MiniProjects/Digital Clock/script.js
@@ -0,0 +1,15 @@
+function updateClock() {
+  const now = new Date();
+  const hours = String(now.getHours()).padStart(2, "0")%12;
+  const minutes = String(now.getMinutes()).padStart(2, "0");
+  const seconds = String(now.getSeconds()).padStart(2, "0");
+  document.getElementById(
+    "clock"
+  ).textContent = `${hours}:${minutes}:${seconds}`;
+}
+
+// Update the clock every second
+setInterval(updateClock, 1000);
+
+// Initial call to display the time immediately
+updateClock();
